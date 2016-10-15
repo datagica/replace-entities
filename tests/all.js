23f7323dcbfbed66ce681d8e33b50ce2af6affee
Julian Bilcke <julian.bilcke@gmail.com>
2016-10-15 11:44:29 +0200

    new pattern replacement function

--- tests/all.js
+++ tests/all.js
@@ -6,57 +6,86 @@ const replace = require("../lib/replace-entities");
 
 describe('@datagica/replace-entities', () => {
 
-  describe('should do basic substitution', () => {
+  describe('should replace entities in a damaged text', () => {
 
-    const input = 'Vegeta, eat your vegetables!';
+    const entities = [{
+      "position": {
+        "score": 1,
+        "index": 1,
+        "end": 60,
+        "ngram": "abcedfghij",
+        "begin": 50
+      }
+    }, {
+      "position": {
+        "score": 1,
+        "index": 2,
+        "end": 110,
+        "ngram": "klmnopqrst",
+        "begin": 100
+      }
+    }];
 
-    const entities = [
-      {
+    // split into random sentences
+    const sentences = [
+      `lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum abcedfghij lorem ipsum `,
+      `lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum klmno`,
+      `pqrst lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum `,
+      `lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum`
+    ];
+
+
+    const sequence = replace({
+      entities: entities,
+      content: sentences
+    });
+
+    expect(sequence).to.be.like([
+      "lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
+      "\n", {
         "position": {
-          "ngram": "Vegeta",
-          "score": "1",
-          "index": 0,
-          "begin": 0,
-          "end": 6
-        },
-        "entity": {
-          "id": "vegeta",
-          "label": "vegeta",
-          "description": "character"
+          "score": 1,
+          "index": 1,
+          "end": 60,
+          "ngram": "abcedfghij",
+          "begin": 50
         }
       },
-      {
+      " lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ",
+      "\n", {
         "position": {
-          "ngram": "vegetables",
-          "score": 0.8,
-          "index": 3,
-          "begin": 17,
-          "end": 27
-        },
-        "entity": {
-          "id": "vegetable",
-          "label": "vegetable",
-          "description": "edible thing"
+          "score": 1,
+          "index": 2,
+          "end": 110,
+          "ngram": "klmnopqrst",
+          "begin": 100
         }
-      }
-    ];
-    const wrapper = item => {
-      return `<a href="/resource/${item.entity.label}" class="${
-      (item.position.score > 0.80) ? 'good' : 'normal'
-      }">${
-      item.position.ngram
-      }</a> <i>(${
-      item.entity.description
-      })</i>`
-    };
+      },
+      " lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum",
+      "\n"
+    ])
 
-    const output = replace(input, entities, wrapper);
+    const transformer = (item) => (
+      item === '\n' ?
+      '<br/>' :
+      typeof item === 'string' ?
+      item :
+      `<a href="/resource/${item.position.ngram}" ` +
+      `class="${(item.position.score > 0.80) ? 'good' : 'normal'}"` +
+      `>${item.position.ngram}</a>`
+    )
+
+    const output = sequence.map(x => transformer(x)).join('')
+
     expect(output).to.be.like(
-      `<a href="/resource/vegeta" class="good">Vegeta</a> ` +
-      `<i>(character)</i>, eat your ` +
-      `<a href="/resource/vegetable" class="normal">vegetables</a> ` +
-      `<i>(edible thing)</i>!`
+      `lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ` +
+      `ipsum <br/><a href="/resource/abcedfghij" class="good">abcedfghij</a> lorem ` +
+      `ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ` +
+      `ipsum <br/><a href="/resource/klmnopqrst" class="good">klmnopqrst</a> lorem ` +
+      `ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum ` +
+      `lorem ipsum lorem ipsum lorem ipsum lorem ipsum lorem ipsum<br/>`
     );
     console.log("done")
   })
+
 })
